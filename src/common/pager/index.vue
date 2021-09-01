0c9035c12dc1ba816e675675a3d196eb4cb9585a
liudong <liudong-ls@bestpay.com.cn>
2021-09-01 13:44:41 +0800

    task: 整理分页器样式

--- src/common/pager/index.vue
+++ src/common/pager/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>{{ res }}</div>
+    <div class="pager-container">
+        <div class="pager-left">
+            <i class="fa fa-angle-left"></i>
+        </div>
+        <ul class="pager-items">
+            <li v-for="(item, index) in countNum" :key="index">{{ item }}</li>
+        </ul>
+        <div class="pager-right">
+            <i class="fa fa-angle-right"></i>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -27,10 +37,56 @@ export default defineComponent({
     setup() {
         const res = ref(useCountPages(20, 9))
         const countNum = _.range(1, res.value + 1)
+        console.log(countNum)
         // const pages = reactive()
-        return { res }
+        return { countNum }
     }
 })
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.pager-container {
+    margin: 10px 0;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+
+    .pager-left, .pager-right {
+        cursor: pointer;
+
+        i {
+            color: #000;
+            font-weight: bold;
+            font-size: 16px;
+        }
+    }
+
+    .pager-items {
+        display: flex;
+        margin: 0;
+        padding: 0 15px;
+        justify-content: flex-start;
+
+        li {
+            font-size: 14px;
+            cursor: pointer;
+            font-weight: bold;
+            transition: all 0.2s;
+            width: 20px;
+            height: 30px;
+            // border: 1px solid #000;
+            box-sizing: border-box;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            &:hover {
+                transform: translateY(-6px);
+                font-size: 16px;
+            }
+        }
+    }
+}
+</style>
