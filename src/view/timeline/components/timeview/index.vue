516d39db86206a5e1768586c62620eb74619b523
liudong <liudong-ls@bestpay.com.cn>
2021-09-17 23:02:26 +0800

    task: 推

--- src/view/timeline/components/timeview/index.vue
+++ src/view/timeline/components/timeview/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="timeview-container">
-    <div class="timeview-container__title">
-      <i class="fa fa-clock-o"></i>{{}}
-    </div>
+    <div class="timeview-container__time"><i class="fa fa-clock-o"></i> {{ timeDate.month }} {{ timeDate.year }}</div>
+    <div class="timeview-container__title">{{ timeDate.title }}</div>
   </div>
 </template>
 
@@ -10,18 +9,36 @@
 import { defineComponent } from 'vue'
 export default defineComponent({
   props: {
-    data: {
-      type: Object,
+    timeDate: {
+      type: Array,
       default() {
-        return {}
-      }
+        return {
+          month: '9',
+          year: '2021',
+          title: '测试标题',
+          dayArray: [
+            {
+              day: '4',
+              
+            }
+          ]
+        }
+      },
+    },
+  },
+  setup(props, ctx) {
+    return {
+      props,
     }
-  }
+  },
 })
 </script>
 
 <style lang="stylus" scoped>
-  .timeview-container__title{
-    font-size: 30px;
-  }
+.timeview-container__time{
+  font-size: 20px;
+}
+.timeview-container__title{
+  font-size: 30px;
+}
 </style>
